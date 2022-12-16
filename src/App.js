b490c2770293ac136efeaf3b4ee32ef6590c9b10
sathgurus <srisathguru13@gmail.com>
2022-12-16 18:48:23 +0530

    login page ui complete

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Login } from './components/login';
+
+function App() {
+  return (
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login/>}/>
+      </Routes>
+
+    </BrowserRouter>
+    
+    
+    </>
+  );
+}
+
+export default App;
